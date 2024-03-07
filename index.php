4db314c530926890296fbe08d72a4dbcdb9f7c8e
ghani-ds <l238027@lhr.nu.edu.pk>
2024-03-07 11:35:40 +0500

    adding Style.css metadata | index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Created by innovate360.
+ * User: Administrator
+ * Date: 2024/3/07
+ * Time: 11:30
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Innovate 360 Theme</title>
+</head>
+
+<body>
+    <h1>
+        Hello Folks! This is Innovate 360 Theme.
+    </h1>
+</body>
+
+</html>
